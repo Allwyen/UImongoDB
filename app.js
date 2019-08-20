301a6c5c40ea3d61841780af852f1fb6138f1d66
Allwyen <allwyen27@gmail.com>
2019-08-20 12:20:42 +0530

    UImongoDB updated

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const Express = require("express");
+const Mongoose = require('mongoose');
 
+var request = require('request');
 var bodyParser = require('body-parser');
 
 var app = new Express();
@@ -9,15 +11,66 @@ app.set('view engine','ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+Mongoose.connect("mongodb://localhost:27017/collegedb");
+
+const StudentModel= Mongoose.model("studentdetail",{
+    uname:String,
+    uroll:String,
+    uadno:String,
+    ucollege:String
+});
+
 app.get('/',(req,res)=>{
     res.render('index');
+})
+
+app.get('/addstudents',(req,res)=>{
+    res.render('add');
 }); 
 
 app.post('/read',(req,res)=>{
     //var items=req.body;
     //res.render('read',{item:items});
-    res.send(req.body);
+
+    var student = new StudentModel(req.body);
+    var result = student.save((error,data)=>{
+        if(error)
+        {
+            throw error;
+            res.send(error);
+        }
+        else
+        {
+            res.send("<script>alert('Successfully Inserted')</script>");
+        }
+    });
+
 });
+
+app.get('/getdatas',(req,res)=>{
+
+    var result = StudentModel.find((error,data)=>{
+        if(error)
+        {
+            throw error;
+            res.send(error);
+        }
+        else
+        {
+            res.send(data);
+        }
+    });
+});
+
+const APIurl = "http://localhost:3456/getdatas";
+
+app.get('/viewstudents',(req,res)=>{
+
+    request(APIurl,(error,response,body)=>{
+        var data = JSON.parse(body);
+        res.render('view',{data:data});
+    });
+})
 
 app.listen(process.env.PORT || 3456,()=>{
     console.log("Server running on port::3456...");
